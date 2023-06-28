4853e032135316acbb00253547fb16cfe1bb0aed
Taylor Davis <taylor.p.d94@hotmail.com>
2023-06-28 15:17:35 -0500

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,60 @@
+import { footerLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+type ColumnProps = {
+  title: string;
+  links: Array<string>;
+};
+
+const FooterColumn = ({ title, links }: ColumnProps) => (
+  <div className="footer_column">
+    <h4 className="font-semibold">{title}</h4>
+    <ul className="flex flex-col gap-2 font-normal">
+      {links.map((link) => (
+        <Link href="/" key={link}>
+          {link}
+        </Link>
+      ))}
+    </ul>
+  </div>
+);
+
+const Footer = () => {
+  return (
+    <footer className="flexStart footer">
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex items-start flex-col">
+          <Image
+            src="/logo-purple.svg"
+            width={115}
+            height={38}
+            alt="Flexibbble"
+          />
+          <p className="text-start text-sm font-normal mt-5 max-w-xs">
+            Flexibble is the world's leading community for creatives to share,
+            grow, and get hired.
+          </p>
+        </div>
+        <div className="flex flex-wrap gap-12">
+          {footerLinks.map((group, i) => (
+            <FooterColumn
+              title={footerLinks[i].title}
+              links={footerLinks[i].links}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="flexBetween footer_copyright">
+        <p>@ 2023 Flexibble. All rights reserved.</p>
+        <p className="text-gray">
+          <span className="text-black font-semibold">10, 214</span> projects
+          submitted
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
